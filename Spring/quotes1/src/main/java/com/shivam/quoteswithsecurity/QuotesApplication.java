6cb0882cfd797daea3aae6bcdb4c3c25b34f5022
Shivamgulia <92909697+Shivamgulia@users.noreply.github.com>
2022-10-03 23:13:05 +0530

    user details remove

--- Spring/quotes1/src/main/java/com/shivam/quoteswithsecurity/QuotesApplication.java
+++ Spring/quotes1/src/main/java/com/shivam/quoteswithsecurity/QuotesApplication.java
@@ -1,18 +1,7 @@
 package com.shivam.quoteswithsecurity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.shivam.quoteswithsecurity.dao.UserDetailsRepository;
-import com.shivam.quoteswithsecurity.entity.Authority;
-import com.shivam.quoteswithsecurity.entity.User;
 
 @SpringBootApplication
 public class QuotesApplication {
@@ -38,36 +27,4 @@ public class QuotesApplication {
 //	}
 	
 	
-//	@PostConstruct
-//	protected void init() {
-//		
-//		List<Authority> authorityList=new ArrayList<>();
-//		
-//		authorityList.add(createAuthority("USER","User role"));
-//		authorityList.add(createAuthority("ADMIN","Admin role"));
-//		
-//		User user=new User();
-//		
-//		user.setUserName("shivam");
-//		user.setFirstName("shivam");
-//		user.setLastName("Gulia");
-//		
-//		user.setPassword(passwordEncoder.encode("shivam@123"));
-//		user.setEnabled(true);
-//		user.setAuthorities(authorityList);
-//		
-//		userDetailsRepository.save(user);
-//		
-//		
-//		
-//	}
-//	
-//	
-//	private Authority createAuthority(String roleCode,String roleDescription) {
-//		Authority authority=new Authority();
-//		authority.setRoleCode(roleCode);
-//		authority.setRoleDescription(roleDescription);
-//		return authority;
-//	}
-
 }
